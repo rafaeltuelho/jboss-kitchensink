0aaefddc479898fbcb1a3083e87490422269b831
Rafael T. C. Soares <rafaelcba@gmail.com>
2018-07-21 18:52:13 -0300

    moving modules to system/layers/openshift as expected by eap71 xpaas builder image

--- openshift/eap71/install.sh
+++ openshift/eap71/install.sh
@@ -4,5 +4,6 @@ injected_dir=$1
 source /usr/local/s2i/install-common.sh
 # no additional deployments here
 #install_deployments ${injected_dir}/injected-deployments.war
+#to system/layers/openshift/
 install_modules ${injected_dir}/modules
 configure_drivers ${injected_dir}/drivers.env
