da5c35a740fce5ce8f737532ab473ad4f8f5abe2
Rafael T. C. Soares <rafaelcba@gmail.com>
2018-07-21 17:55:52 -0300

    adding JDBC drivers

---
+++ openshift/eap71/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+injected_dir=$1
+source /usr/local/s2i/install-common.sh
+# no additional deployments here
+#install_deployments ${injected_dir}/injected-deployments.war
+install_modules ${injected_dir}/modules
+configure_drivers ${injected_dir}/drivers.env
