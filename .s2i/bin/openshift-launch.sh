e522fd01bdf685cf5d2834512cb71af01fa6d2cd
Rafael T. C. Soares <rafaelcba@gmail.com>
2018-07-23 18:13:34 -0300

    removing calls to script logging.sh

--- .s2i/bin/openshift-launch.sh
+++ .s2i/bin/openshift-launch.sh
@@ -2,14 +2,13 @@
 # Openshift EAP launch script
 
 source ${JBOSS_HOME}/bin/launch/openshift-common.sh
-source $JBOSS_HOME/bin/launch/logging.sh
 
 echo -e "\n\n"
-log_warning "This is a fork of the original $JBOSS_HOME/bin/openshift-launch.sh found in app source repo (.s2i directory)!!!"
+echo "This is a fork of the original $JBOSS_HOME/bin/openshift-launch.sh found in app source repo (.s2i directory)!!!"
 
 # TERM signal handler
 function clean_shutdown() {
-  log_error "*** JBossAS wrapper process ($$) received TERM signal ***"
+  echo "*** JBossAS wrapper process ($$) received TERM signal ***"
   $JBOSS_HOME/bin/jboss-cli.sh -c ":shutdown(timeout=60)"
   wait $!
 }
@@ -21,16 +20,16 @@ if [ "${SPLIT_DATA^^}" = "TRUE" ]; then
 
   partitionPV "${DATA_DIR}" "${SPLIT_LOCK_TIMEOUT:-30}"
 else
-  log_warning "$JBOSS_HOME/bin/launch/configure.sh already called by app's run script"
+  echo "$JBOSS_HOME/bin/launch/configure.sh already called by app's run script"
   #source $JBOSS_HOME/bin/launch/configure.sh
 
-  log_info "Running $JBOSS_IMAGE_NAME image, version $JBOSS_IMAGE_VERSION"
+  echo "Running $JBOSS_IMAGE_NAME image, version $JBOSS_IMAGE_VERSION"
 
   trap "clean_shutdown" TERM
 
   if [ -n "$CLI_GRACEFUL_SHUTDOWN" ] ; then
     trap "" TERM
-    log_info "Using CLI Graceful Shutdown instead of TERM signal"
+    echo "Using CLI Graceful Shutdown instead of TERM signal"
   fi
 
   $JBOSS_HOME/bin/standalone.sh -c standalone-openshift.xml -bmanagement 127.0.0.1 ${JAVA_PROXY_OPTIONS} ${JBOSS_HA_ARGS} ${JBOSS_MESSAGING_ARGS} &
